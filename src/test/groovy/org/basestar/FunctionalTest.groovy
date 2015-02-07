d8434fec901ecbbbfa286062943b2534081d2acb
Connor Garvey <connorwgarvey@yahoo.com>
2015-02-07 13:28:21 -0800

    Added ability to run tests

---
+++ src/test/groovy/org/basestar/FunctionalTest.groovy
@@ -0,0 +1,12 @@
+package org.basestar
+
+import spock.lang.Specification
+
+/**
+ * @since February 07, 2015
+ */
+class FunctionalTest extends Specification {
+    def 'passes'() {
+        expect: true
+    }
+}
